f488676400149bf405782eea8140ef5baec65c11
Mayankm96 <mayankmittal155@gmail.com>
2017-10-05 00:08:36 +0530

    pass arg through roslaunch

--- debug_module/alpha_dashboard/scripts/rqt_alpha_console.py
+++ debug_module/alpha_dashboard/scripts/rqt_alpha_console.py
@@ -4,9 +4,13 @@ import sys
 import subprocess
 import rospy
 
+## Alternate approach by using roslaunchAPIs for python
+# See here: http://wiki.ros.org/roslaunch/API%20Usage
+
+# Approach stated here: https://answers.ros.org/question/272267/is-it-possible-to-launch-non-ros-applications-with-roslaunch/
 if __name__=="__main__":
-    if len(sys.argv) >2:
+    if len(sys.argv) > 2:
         print("Usage: rqt_alpha_console.py file")
     else
-        command = "rqt --perspective-file" + sys.argv[1]
-        sys.exit(subprocess.call(command)
+        command = "rqt --perspective-file" + (rospy.myargv(argv=sys.argv)[1])
+        sys.exit(subprocess.call(command))
